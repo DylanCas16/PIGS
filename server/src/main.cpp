6198172a02e8038b4da87c89f8a4540df7064d35
DylanCas16 <dylancastrillon@outlook.com>
2026-03-25 20:58:00 +0000

    Created the methods for items in a supply list

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -78,14 +78,41 @@ int main() {
         }
     });
 
+    // DELETE: Delete a supply list by its ID
+    svr.Delete(R"(/api/supplies/([^/]+))", [](const httplib::Request& req, httplib::Response& res){
+        set_cors(res);
+        try
+        {
+            std::string const supply_id = req.matches[1];
+            std::cout << "Trying to delete the supply list: " << supply_id << std::endl;
+
+            httplib::Client cli(FIREBASE_HOST);
+            cli.enable_server_certificate_verification(false);
+
+            std::string const url = "/supplies/" + supply_id + ".json";
+            auto res_fb = cli.Delete(url);
+
+            if (res_fb && res_fb->status == 200) {
+                res.status = 200;
+                res.set_content(R"({"message": "Supply list deleted correctly"})", "application/json");
+            } else
+            {
+                res.status = 500;
+                res.set_content(R"({"error": "Error trying to delete the supply list"})", "application/json");
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+            res.status = 500;
+            res.set_content(R"({"error": "Server error"})", "application/json");
+        }
+    });
+
     // POST: Add an item to a specific supply list
-    svr.Post("/api/items", [](const httplib::Request& req, httplib::Response& res) {
+    svr.Post("/api/supplies/([^/]+)/items/?", [](const httplib::Request& req, httplib::Response& res) {
         set_cors(res);
         try {
             auto data = json::parse(req.body);
-            std::string const supply_id = data["supply_id"];
-
-            data.erase("supply_id");
+            std::string const supply_id = req.matches[1];
 
             httplib::Client cli(FIREBASE_HOST);
             cli.enable_server_certificate_verification(false);
@@ -103,27 +130,51 @@ int main() {
         }
     });
 
-    // DELETE: Delete a supply list by its ID
-    svr.Delete(R"(/api/supplies/([^/]+))", [](const httplib::Request& req, httplib::Response& res){
+    //PUT: updating an item from a list.
+    svr.Put(R"(/api/supplies/([^/]+)/items/([^/]+))", [](const httplib::Request& req, httplib::Response& res){
         set_cors(res);
-        try
-        {
+        try {
+            auto data = json::parse(req.body);
             std::string const supply_id = req.matches[1];
-            std::cout << "Trying to delete the supply list: " << supply_id << std::endl;
+            std::string const item_id = req.matches[2];
 
             httplib::Client cli(FIREBASE_HOST);
             cli.enable_server_certificate_verification(false);
 
-            std::string const url = "/supplies/" + supply_id + ".json";
+            std::string const url = "/supplies/" + supply_id + "/items/" + item_id + ".json";
+            auto res_fb = cli.Put(url, data.dump(), "application/json");
+
+            if (res_fb && res_fb->status == 200) {
+                res.status = 200;
+                res.set_content(res_fb->body, "application/json");
+            }
+        } catch (...) {
+            res.status = 400;
+            res.set_content(R"({"error": "Missing data or JSON invalid"})", "application/json");
+        }
+    });
+
+    // DELETE: delete an item from a list.
+    svr.Delete(R"(/api/supplies/([^/]+)/items/([^/]+))", [](const httplib::Request& req, httplib::Response& res){
+        set_cors(res);
+        try {
+            std::string const supply_id = req.matches[1];
+            std::string const item_id = req.matches[2];
+            std::cout << "Trying to delete an item: " << item_id << std::endl;
+
+            httplib::Client cli(FIREBASE_HOST);
+            cli.enable_server_certificate_verification(false);
+
+            std::string const url = "/supplies/" + supply_id + "/items/" + item_id + ".json";
             auto res_fb = cli.Delete(url);
 
             if (res_fb && res_fb->status == 200) {
                 res.status = 200;
-                res.set_content(R"({"message": "Supply list eliminated correctly"})", "application/json");
+                res.set_content(R"({"message": "Item deleted correctly"})", "application/json");
             } else
             {
                 res.status = 500;
-                res.set_content(R"({"error": "Error trying to delete the supply list"})", "application/json");
+                res.set_content(R"({"error": "Error trying to delete the item"})", "application/json");
             }
         } catch (const std::exception& e) {
             std::cerr << "Exception: " << e.what() << std::endl;
